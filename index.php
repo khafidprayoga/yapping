1b495cefc57828b68d71bfb4a967c9af4f83e850
Khafid Prayoga <khafidp@pm.me>
2025-01-16 10:57:28 +0700

    change define to const and add unique constraint on User.username

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 declare(strict_types=1);
-define('APP_ROOT', __DIR__);
+const APP_ROOT = __DIR__;
 
-ini_set('display_errors', 1);
+//ini_set('log_errors', "1");
+//ini_set('display_errors', "1");
+//ini_set("date.timezone", "Asia/Jakarta");
+
+error_reporting(E_ALL);
 require __DIR__ . '/vendor/autoload.php';
 
 use Khafidprayoga\PhpMicrosite\Routes\Route;
--- src/Models/Entities/User.php
+++ src/Models/Entities/User.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'users')]
+#[ORM\UniqueConstraint(name: 'username', columns: ['username'])]
 class User
 {
     #[ORM\Id]
